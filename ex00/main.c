a3e1b8a36087febbac261af0485371795e99789b
Leonardo Pedro Silveira <lpedro-s@c2r7p2.42sp.org.br>
2026-02-01 09:18:05 -0300

    norminette

--- ex00/main.c
+++ ex00/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpedro-s <lpedro-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 10:25:59 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/02/01 09:08:37 by fsayuri-         ###   ########.fr       */
+/*   Updated: 2026/02/01 09:11:35 by lpedro-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 int	main(int argc, char **argv)
 {
-	char *file;
-	int num;
+	char	*file;
+	int		num;
 
 	if (!ft_arg_validation(argc, argv))
 	{
 		ft_error_input();
 		return (0);
 	}
-
 	file = ft_file_name(argc, argv);
 	num = ft_str_to_int(argc, argv);
 	if (!ft_read_file(file))
